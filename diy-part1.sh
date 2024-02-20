601ebca003c7e59660739e19bf8695623f40e569
CrazyPegasus <38367520+CrazyPegasus@users.noreply.github.com>
2024-02-20 19:57:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git crazy https://github.com/CrazyPegasus/crazyweibs-packages' >>feeds.conf.default
+echo 'src-git crazy https://github.com/kiddin9/openwrt-packages.git' >>feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
